8fa18bf15d910551645a52f5f53bcd1757a971b9
ali-saeed91 <engineer.ali91@live.com>
2023-01-01 02:46:21 +0500

    updating for Render

--- app.py
+++ app.py
@@ -9,14 +9,14 @@ load_dotenv()
 def create_app():
     app = Flask(__name__)
     client = MongoClient(os.environ.get("MONGODB_URI"))
-    app.db = client.microblog
+    db = client.microblog
 
     @app.route("/", methods=["GET", "POST"])
     def home():
         if request.method == "POST":
             entry_content = request.form.get("content")
             formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
-            app.db.entries.insert({"content": entry_content, "date": formatted_date})
+            db.entries.insert({"content": entry_content, "date": formatted_date})
         
         entries_with_date = [
             (
@@ -24,7 +24,7 @@ def create_app():
                 entry["date"],
                 datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
             )
-            for entry in app.db.entries.find({})
+            for entry in db.entries.find({})
         ]
         return render_template("home.html", entries=entries_with_date)
     
