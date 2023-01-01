39410ea5b0ffed3c7f94e49a051ad42001596091
ali-saeed91 <engineer.ali91@live.com>
2023-01-01 05:41:30 +0500

    Testing Render with Custom

--- myapp.py
+++ myapp.py
@@ -10,23 +10,25 @@ load_dotenv()
 app = Flask(__name__)
 client = MongoClient(os.environ.get("MONGODB_URI"))
 app.db = client.microblog
+entries=[]
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         entry_content = request.form.get("content")
         formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
-        app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
-    
-    # entries_with_date = [
-    #     (
-    #         entry["content"],
-    #         entry["date"],
-    #         datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
-    #     )
-    #     for entry in app.db.entries.find({})
-    # ]
-    return render_template("home.html")
-# entries=entries_with_date
+        # app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
+        entries.append({"content": entry_content, "date": formatted_date})
+        
+    entries_with_date = [
+            (
+                entry["content"],
+                entry["date"],
+                datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
+            )
+            # for entry in app.db.entries.find({})
+            for entry in entries
+        ]
+    return render_template("home.html", entries=entries_with_date)
     
     # return app
